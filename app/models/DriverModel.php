f220547fb96e732b593074919977b12278c42b0e
DasunThathsara <dasun.thathsara.sri@gmail.com>
2023-09-29 14:18:29 +0530

    Update database

--- app/models/DriverModel.php
+++ app/models/DriverModel.php
@@ -6,9 +6,73 @@ class DriverModel{
         $this->db = new Database();
     }
 
-    public function getUser(){
-        $this->db->query('SELECT * FROM users');
+    // Register user
+    public function register($data): bool
+    {
+        // Prepare statement
+        $this->db->query('INSERT INTO user (name, username, email, password, userType, contactNo) VALUES (:name, :username, :email, :password, :userType, :contactNo)');
 
-        return $this->db->resultSet();
+        // Bind values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':username', $data['username']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':userType', $data['user_type']);
+        $this->db->bind(':contactNo', $data['contact_no']);
+
+        // Execute
+        if ($this->db->execute()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // Find user
+    public function findUserByEmail($email): bool
+    {
+        $this->db->query('SELECT * FROM user WHERE email = :email');
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->single();
+
+        // Check row
+        if ($this->db->rowCount() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function findUserByUsername($username): bool
+    {
+        $this->db->query('SELECT * FROM user WHERE username = :username');
+        $this->db->bind(':username', $username);
+
+        $row = $this->db->single();
+
+        // Check row
+        if ($this->db->rowCount() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Login user
+    public function login($email, $password, $username){
+        $this->db->query('SELECT * FROM user WHERE email = :email OR username = :username');
+        $this->db->bind(':email', $email);
+        $this->db->bind(':username', $username);
+
+        $row = $this->db->single();
+
+        $hashed_password = $row->password;
+        if (password_verify($password, $hashed_password)){
+            return $row;
+        } else {
+            return false;
+        }
     }
 }
