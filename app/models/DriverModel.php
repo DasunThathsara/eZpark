c092bb0c2896d35182592c1bb303000255d36c2a
DasunThathsara <dasun.thathsara.sri@gmail.com>
2023-09-30 16:58:13 +0530

    Complete driver's vehicle CRUD

--- app/models/DriverModel.php
+++ app/models/DriverModel.php
@@ -52,4 +52,47 @@ class DriverModel{
 
         return $row;
     }
+
+    public function removeVehicle($data): bool
+    {
+        // Prepare statement
+        $this->db->query('DELETE FROM vehicle WHERE name = :name AND id = :id');
+
+        // Bind values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':id', $_SESSION['user_id']);
+        print_r($data['name']);
+        print_r($_SESSION['user_id']);
+        // Execute
+        if ($this->db->execute()){
+            print_r("check 4");
+            return true;
+        }
+        else {
+            print_r("check 5");
+            return false;
+        }
+    }
+
+    public function updateVehicle($data): bool
+    {
+        // Prepare statement
+        $this->db->query('UPDATE vehicle SET name = :name, vehicleType = :vehicleType WHERE id = :id and name = :old_name');
+
+        // Bind values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':old_name', $data['old_name']);
+        $this->db->bind(':vehicleType', $data['vehicle_type']);
+        $this->db->bind(':id', $_SESSION['user_id']);
+
+
+        print_r($data);
+        // Execute
+        if ($this->db->execute()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- app/views/driver/vehicles/update.php
+++ app/views/driver/vehicles/update.php
@@ -1,1 +1,55 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+<!--  TOP NAVIGATION  -->
+<?php require APPROOT.'/views/inc/components/topnavbar.php'; ?>
+
+<!--  SIDE NAVIGATION  -->
 <?php
+$section = 'vehicles';
+require APPROOT.'/views/inc/components/sidenavbar.php';
+?>
+
+<div class="form-container">
+    <h1>Update Vehicle</h1>
+    <?php if (!empty($data['err'])){?>
+        <div class="error-msg">
+            <span class="form-invalid"><?php echo $data["err"] ?></span>
+        </div>
+    <?php } ?>
+
+    <form action="<?php echo URLROOT ?>/driver/vehicleUpdate" method="post">
+        <!-- Name -->
+        <div class="form-input-title">Name:</div>
+        <input type="text" name="name" id="name" required value="<?php echo $data['name'] ?>" />
+        <input type="text" name="old_name" id="old_name" required value="<?php echo $data['name'] ?>"  disable hidden />
+
+        <br><br>
+        <!-- Vehicle Type -->
+        <div class="form-input-title">User Type:</div>
+        <div class="user-selection-container">
+            <ul class="user-selection-list">
+                <li data-user-type="car">Car</li>
+                <li data-user-type="bike">Bike</li>
+                <li data-user-type="3wheel">3wheel</li>
+            </ul>
+        </div>
+
+        <!-- Hidden Input for User Type -->
+        <input type="hidden" name="vehicle_type" id="vehicle_type" value="<?php echo $data['vehicle_type'] ?>" />
+
+        <br><br>
+
+        <!-- Submit -->
+        <input type="submit" value="Update">
+    </form>
+</div>
+
+<script>
+    const userSelectionList = document.querySelector('.user-selection-list');
+
+    userSelectionList.addEventListener('click', function(event) {
+        if (event.target.tagName === 'LI') {
+            document.getElementById('vehicle_type').value = event.target.getAttribute('data-user-type');
+        }
+    });
+</script>
+<?php require APPROOT.'/views/inc/footer.php'; ?>
