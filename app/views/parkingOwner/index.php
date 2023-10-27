6f6e43c5f213a19c2093780a562dad0cbbfb3276
GaminduJR <gamindujr@gmail.com>
2023-10-27 17:12:07 +0530

    add some dashboard for parking owner

--- app/views/parkingOwner/index.php
+++ app/views/parkingOwner/index.php
@@ -12,6 +12,92 @@
     <section class="section" id="main">
         <div class="container">
             <h1>Parking Owner Dashboard</h1>
+            <div class="cards">
+                <!-- Card 1 -->
+                <div class="card">
+                    <a href="#">
+                        <table>
+                            <tr>
+                                <th>
+                                    <img src="<?php echo URLROOT ?>/images/vehicle.svg" alt="">
+                                </th>
+                                <td>
+                                    <p>0</p>
+                                    <p>Today's Vehicle Entries</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </a>
+                </div>
+                
+                <!-- Card 2 -->
+                <div class="card">
+                    <a href="#">
+                        <table>
+                            <tr>
+                                <th>
+                                    <img src="<?php echo URLROOT ?>/images/vehicle.svg" alt="">
+                                </th>
+                                <td>
+                                    <p>0</p>
+                                    <p>Yesterday's Vehicle Entries</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </a>
+                </div>
+
+                <!-- Card 3 -->
+                <div class="card">
+                    <a href="#">
+                        <table>
+                            <tr>
+                                <th>
+                                    <img src="<?php echo URLROOT ?>/images/vehicle.svg" alt="">
+                                </th>
+                                <td>
+                                    <p>3</p>
+                                    <p>Last 7 days Vehicle Entries</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </a>
+                </div>
+
+                <!-- Card 4 -->
+                <div class="card">
+                    <a href="#">
+                        <table>
+                            <tr>
+                                <th>
+                                    <img src="<?php echo URLROOT ?>/images/vehicle.svg" alt="">
+                                </th>
+                                <td>
+                                    <p>8</p>
+                                    <p>Total Vehicle Entries</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </a>
+                </div>
+
+                <!-- Card 5 -->
+                <div class="card">
+                    <a href="#">
+                        <table>
+                            <tr>
+                                <th>
+                                    <img src="<?php echo URLROOT ?>/images/useridenty.png" alt="">
+                                </th>
+                                <td>
+                                    <p>3</p>
+                                    <p>Total Registered Users</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </a>
+                </div>
+            </div>
         </div>
     </section>
 </main>
