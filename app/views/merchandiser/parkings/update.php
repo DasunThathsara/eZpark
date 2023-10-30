196789a4ad1ce4dbfa6303aa60e6558a66567bea
GaminduJR <gamindujr@gmail.com>
2023-10-30 22:31:52 +0530

    add some filed atributes for mechandiser land update

--- app/views/merchandiser/parkings/update.php
+++ app/views/merchandiser/parkings/update.php
@@ -30,6 +30,49 @@ require APPROOT.'/views/inc/components/sidenavbar.php';
 
         <br><br>
 
+        <!-- Street -->
+        <div class="form-input-title">Street:</div>
+        <input type="text" name="street" id="street" required value="<?php echo $data['street'] ?>" />
+        <input type="text" name="old_street" id="old_street" required value="<?php echo $data['street'] ?>"  disable hidden />
+
+        <br><br>
+
+        <!-- Deed -->
+        <div class="form-input-title">Deed:</div>
+        <input type="text" name="deed" id="deed" required value="<?php echo $data['deed'] ?>" />
+        <input type="text" name="old_deed" id="old_deed" required value="<?php echo $data['deed'] ?>"  disable hidden />
+
+        <br><br>
+
+        <!-- car -->
+        <div class="form-input-title">How much the Car parking slots:</div>
+        <input type="text" name="car" id="car" required value="<?php echo $data['car'] ?>" />
+        <input type="text" name="old_car" id="old_car" required value="<?php echo $data['car'] ?>"  disable hidden />
+
+        <br><br>
+
+        <!-- Bike -->
+        <div class="form-input-title">How much the Bike parking slots:</div>
+        <input type="text" name="bike" id="bike" required value="<?php echo $data['bike'] ?>" />
+        <input type="text" name="old_bike" id="old_bike" required value="<?php echo $data['bike'] ?>"  disable hidden />
+
+        <br><br>
+
+        <!-- Three Wheel -->
+        <div class="form-input-title">How much the Three Wheel parking slots:</div>
+        <input type="text" name="threeWheel" id="threeWheel" required value="<?php echo $data['threeWheel'] ?>" />
+        <input type="text" name="old_threeWheel" id="old_threeWheel" required value="<?php echo $data['threeWheel'] ?>"  disable hidden />
+
+        <br><br>
+
+        <br><br>
+        <!-- contactNo -->
+        <div class="form-input-title">Contact Number:</div>
+        <input type="text" name="contactNo" id="contactNo" required value="<?php echo $data['contactNo'] ?>" />
+        <input type="text" name="old_contactNo" id="old_contactNo" required value="<?php echo $data['contactNo'] ?>"  disable hidden />
+
+        <br><br>
+
         <!-- Submit -->
         <input type="submit" value="Update">
     </form>
