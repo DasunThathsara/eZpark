04e9d2df7895a208d2befca98afcc553faa3ffe5
GaminduJR <gamindujr@gmail.com>
2023-11-02 23:13:40 +0530

    Added validation  parking owner Model

--- app/models/ParkingOwnerModel.php
+++ app/models/ParkingOwnerModel.php
@@ -51,6 +51,87 @@ class ParkingOwnerModel{
             return false;
         }
     }
+
+    public function findLandID($name){
+        $this->db->query('SELECT * FROM land WHERE name = :name and uid = :uid');
+        $this->db->bind(':name', $name);
+        $this->db->bind(':uid', $_SESSION['user_id']);
+
+        $row = $this->db->single();
+        return $row->id;
+    }
+
+    public function setPrice($data):bool
+    {
+        if ($this->setCarPrice($data) and $this->setBikePrice($data) and $this->setThreeWheelPrice($data)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function setCarPrice($data):bool
+    {
+        $id = (int)$this->findLandID($data['name']);
+
+        // Prepare statement
+        $this->db->query('INSERT INTO price (pid, vehicleType, hourPrice, additionalHourPrice) VALUES (:pid, :vehicleType, :hourPrice, :additionalHourPrice)');
+        $this->db->bind(':pid', $id);
+        $this->db->bind(':vehicleType', 'car');
+        $this->db->bind(':hourPrice', $data['car']);
+        $this->db->bind(':additionalHourPrice', 1);
+
+        // Execute
+        if ($this->db->execute()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public function setBikePrice($data):bool
+    {
+        $id = (int)$this->findLandID($data['name']);
+
+        // Prepare statement
+        $this->db->query('INSERT INTO price (pid, vehicleType, hourPrice, additionalHourPrice) VALUES (:pid, :vehicleType, :hourPrice, :additionalHourPrice)');
+        $this->db->bind(':pid', $id);
+        $this->db->bind(':vehicleType', 'bike');
+        $this->db->bind(':hourPrice', $data['bike']);
+        $this->db->bind(':additionalHourPrice', 1);
+
+        // Execute
+        if ($this->db->execute()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public function setThreeWheelPrice($data):bool
+    {
+        $id = (int)$this->findLandID($data['name']);
+
+        // Prepare statement
+        $this->db->query('INSERT INTO price (pid, vehicleType, hourPrice, additionalHourPrice) VALUES (:pid, :vehicleType, :hourPrice, :additionalHourPrice)');
+        $this->db->bind(':pid', $id);
+        $this->db->bind(':vehicleType', 'threeWheel');
+        $this->db->bind(':hourPrice', $data['threeWheel']);
+        $this->db->bind(':additionalHourPrice', 1);
+
+        // Execute
+        if ($this->db->execute()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
     // Find land
     public function findLandByName($name): bool
     {
@@ -189,6 +270,7 @@ class ParkingOwnerModel{
             return true;
         }
         else {
+
             return false;
         }
     }
