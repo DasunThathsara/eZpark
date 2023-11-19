35111f65ceac51f66556b5c55a046bae5123cab1
DasunThathsara <dasun.thathsara.sri@gmail.com>
2023-11-19 18:33:16 +0530

    Update profile photo upload and remove

--- app/views/inc/components/topnavbar.php
+++ app/views/inc/components/topnavbar.php
@@ -6,7 +6,13 @@
                 <a class="item" href="<?php echo URLROOT ?>/users/register">Register</a>
             <?php }else{ ?>
                 <a class="item logo" onclick="navToggle()"><img style="width: 20px" src="<?php echo URLROOT ?>/images/menu.svg" alt=""></i></a>
-                <a class="item" href="<?php echo URLROOT ?>/users/viewProfile"><img src="<?php echo URLROOT ?>/images/user.png" style="width: 30px" alt="<?php echo $_SESSION['user_name'] ?>"></a>
+
+                <?php if($_SESSION['profile_photo']){ ?>
+                    <a class="item" href="<?php echo URLROOT ?>/users/viewProfile"><img src="<?php echo URLROOT ?>/profile_pics/<?php echo $_SESSION['profile_photo']?>" style="width: 30px; height: 30px; border-radius: 50%" alt="<?php echo $_SESSION['user_name'] ?>"></a>
+                <?php }
+                else{ ?>
+                    <a class="item" href="<?php echo URLROOT ?>/users/viewProfile"><img src="<?php echo URLROOT ?>/images/user.png" style="width: 30px; height: 30px; border-radius: 50%" alt="<?php echo $_SESSION['user_name'] ?>"></a>
+                <?php } ?>
             <?php } ?>
         </div>
     </div>
