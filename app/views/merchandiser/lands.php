5457dc8f1ebce055e9c568c8b3d82a9863d1ef87
GaminduJR <gamindujr@gmail.com>
2023-12-28 19:28:32 +0530

    change merchandiser lands page

--- app/views/merchandiser/lands.php
+++ app/views/merchandiser/lands.php
@@ -11,82 +11,222 @@
 <main class="page-container">
     <section class="section" id="main">
         <div class="container">
-            <h1>Parkings</h1>
+            <h1 class="title">All Lands</h1>
+            <p class="subtitle">View and Edit information related to Lands</p>
 
             <br><br>
-            <a href="<?php echo URLROOT ?>/merchandiser/landRegister" style="font-weight: 1000; font-size: 20px">+</a>
+            <a class="add-btn" href="<?php echo URLROOT ?>/land/landRegister">Add new <span>+</span></a>
 
-            <?php if (sizeof($data) == 0) {?>
-                <div class="emptyLand">You have no any registered parkings</div>
-            <?php }
-            else {?>
-                <div class="table-container">
-                    <table class="table">
-                        <tr>
-                            <th>Parking Name</th>
-                            <th>City</th>
-                            <th>Street</th>
-                            <th>Deed</th>
-                            <th>No. of Cars</th>
-                            <th>No. of Bikes</th>
-                            <th>No. of Three Wheels</th>
-                            <th>Contact No.</th>
-                            <th width="60px"></th>
-                        </tr>
-                        <?php for ($i = 0; $i < sizeof($data); $i++) {?>
-                            <tr>
-                                <td>
-                                    <?php echo $data[$i]->name ?>
-                                </td>
-                                <td>
-                                    <?php echo $data[$i]->city ?>
-                                </td>
-                                <td>
-                                    <?php echo $data[$i]->street ?>
-                                </td>
-                                <td>
-                                    <?php echo $data[$i]->deed ?>
-                                </td>
-                                <td>
-                                    <?php echo $data[$i]->car ?>
-                                </td>
-                                <td>
-                                    <?php echo $data[$i]->bike ?>
-                                </td>
-                                <td>
-                                    <?php echo $data[$i]->threeWheel ?>
-                                </td>
-                                <td>
-                                    <?php echo $data[$i]->contactNo ?>
-                                </td>
-                                <td style="text-align: center">
-                                    <form action="<?php echo URLROOT ?>/merchandiser/landUpdateForm" method="post">
-                                        <input type="text" name="name" id="name" hidden value="<?php echo $data[$i]->name ?>" />
-                                        <input type="text" name="city" id="city" hidden value="<?php echo $data[$i]->city ?>" />
-                                        <input type="text" name="street" id="street" hidden value="<?php echo $data[$i]->street ?>" />
-                                        <input type="text" name="deed" id="deed" hidden value="<?php echo $data[$i]->deed ?>" />
-                                        <input type="number" name="car" id="car" hidden value="<?php echo $data[$i]->car ?>" />
-                                        <input type="number" name="bike" id="bike" hidden value="<?php echo $data[$i]->bike ?>" />
-                                        <input type="number" name="threeWheel" id="threeWheel" hidden value="<?php echo $data[$i]->threeWheel ?>" />
-                                        <input type="number" name="contactNo" id="contactNo" hidden value="<?php echo $data[$i]->contactNo ?>" />
-                                        <input type="submit" class="sub-option" value="Update"/>
-                                    </form>
-                                    <form action="<?php echo URLROOT ?>/merchandiser/landRemove" method="post">
-                                        <input type="text" name="name" id="name" hidden value="<?php echo $data[$i]->name ?>" />
-                                        <input type="submit" class="sub-option" onclick="return confirmSubmit();" value="Delete"/>
-                                    </form>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                    </table>
-                </div>
-            <?php } ?>
+            <div class="content-body">
+                <?php if (sizeof($data) == 0) {?>
+                    <div class="emptyLand">You have no any registered lands</div>
+                <?php }
+                else {?>
+                    <div class="title-options">
+                        <div class="all-lands option-item option-item-active">All Lands</div>
+                        <div class="available-lands option-item">Available</div>
+                        <div class="unavailable-lands option-item">Unavailable</div>
+                    </div>
+                    <hr class="option-break" />
+                    <!-- Search area -->
+                    <div class="search-area">
+                        <!-- Search bar -->
+                        <input type="search" class="data-search" placeholder="Search land...">
+                        <div class="filter-area">
+                            <img class="filter-btn" src="<?php echo URLROOT?>/images/filter-ico.png" alt="">
+                        </div>
+                    </div>
+
+                    <div class="user-card-title">
+                        <div class="name">Name</div>
+                        <div class="location">Location</div>
+                        <div class="capacity">Capacity</div>
+                        <div class="status">Status</div>
+                    </div>
+
+                    <div class="card-set-container">
+                        <!-- Card set -->
+                        <div class="user-cards"></div>
+                        <template class="data-user-template">
+                            <div class="card">
+                                <a href="" class="tile">
+                                    <table>
+                                        <tr>
+                                            <td class="name-td" data-header>
+                                                <p class="name"></p>
+                                                <p class="id-p">Land ID: <span class="id"></span></p>
+                                            </td>
+                                            <td class="location" data-header></td>
+                                            <td class="capacity" data-header></td>
+                                            <td class="status-td" data-header><span class="status"></span></td>
+
+                                            <td class="options">
+                                                <form action="<?php echo URLROOT ?>/land/prices" method="get" class="price-form">
+                                                    <input type="text" name="id" id="id" hidden value="" />
+                                                    <input type="text" name="name" id="name" hidden value="" />
+                                                    <button type="submit" class="price">
+                                                        <img src="<?php echo URLROOT ?>/images/price.svg" alt="">
+                                                    </button>
+                                                </form>
+                                                &nbsp;
+                                                <form action="<?php echo URLROOT ?>/land/landUpdateForm" method="get" class="update-form">
+                                                    <input type="text" name="name" id="name" hidden value="" />
+                                                    <input type="text" name="id" id="id" hidden value="" />
+                                                    <button type="submit" class="edit">
+                                                        <img src="<?php echo URLROOT ?>/images/edit-solid.svg" alt="">
+                                                    </button>
+                                                </form>
+                                                &nbsp;
+                                                <form action="<?php echo URLROOT ?>/land/landRemove" method="post" class="delete-form">
+                                                    <input type="text" name="name" id="name" hidden value="" />
+                                                    <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                        <img src="<?php echo URLROOT ?>/images/trash-solid.svg" alt="">
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </a>
+                            </div>
+                        </template>
+                    </div>
+                <?php } ?>
+            </div>
         </div>
     </section>
 </main>
+
 <script>
     function confirmSubmit() {
-        return confirm("Are you sure you want to delete this parking?");
+        return confirm("Are you sure you want to delete this land?");
     }
+
+    // ------------------------------- Search Bar -------------------------------
+    const userCardTemplate = document.querySelector(".data-user-template");
+    const searchInput = document.querySelector(".data-search");
+
+    searchInput.addEventListener("input", (data) => {
+        const value = data.target.value.toLowerCase();
+        lands.forEach(land => {
+            const isVisible = land.name.toLowerCase().includes(value) || land.city.toLowerCase().includes(value) || land.street.toLowerCase().includes(value);
+            if (land.element) {
+                land.element.classList.toggle("hide", !isVisible);
+            }
+        });
+    });
+
+    let lands = [];
+    var backendData = <?php echo json_encode($data); ?>;
+
+    lands = backendData.map(land => {
+        const card = userCardTemplate.content.cloneNode(true).children[0];
+        console.log(card);
+        card.querySelector(".name").textContent = land.name;
+        card.querySelector(".location").textContent = land.city;
+        card.querySelector(".capacity").textContent = land.car + land.bike + land.threeWheel;
+        card.querySelector(".id").textContent = land.id;
+        if (land.availability === 0){
+            card.querySelector(".status").textContent = 'Unavailable';
+            card.querySelector(".status").classList.add("status-unavailable");
+        }else{
+
+            card.querySelector(".status").textContent = 'Available';
+            card.querySelector(".status").classList.add("status-available");
+        }
+
+        document.querySelector(".user-cards").appendChild(card);
+        const tileLink = card.querySelector('.tile');
+    
+        // Set the parking view link
+        if (tileLink) {
+            tileLink.href = `gotoLand/${land.id}`;
+        } else {
+            console.error("Anchor element with class 'tile' not found in the cloned card:", card);
+        }
+        
+        // Set id and name to go to the price page
+        const priceForm = card.querySelector('.price-form');
+        if (priceForm) {
+            const idInput = priceForm.querySelector('#id');
+            const nameInput = priceForm.querySelector('#name');
+        
+            if (idInput && nameInput) {
+                idInput.value = land.id;
+                nameInput.value = land.name;
+            } else {
+                console.error("Form inputs with id 'id' or 'name' not found in the cloned card:", card);
+            }
+        }
+
+
+        // Set id and name to delete the land
+        const deleteForm = card.querySelector('.delete-form');
+        if (deleteForm) {
+            const nameInput = deleteForm.querySelector('#name');
+            
+            if (nameInput) {
+                nameInput.value = land.name; // Set the value dynamically
+            } else {
+                console.error("Form input with id 'name' not found in the cloned card:", card);
+            }
+        }
+
+
+        // Set values to go to the update page
+        const updateForm = card.querySelector('.update-form');
+        if (updateForm) {
+            const idInput = updateForm.querySelector('#id');
+            const nameInput = updateForm.querySelector('#name');
+            
+            if (nameInput && idInput) {
+                nameInput.value = land.name;
+                idInput.value = land.id;
+            } else {
+                console.error("One or more form inputs not found in the cloned card:", card);
+            }
+        }
+
+        return { id: land.id, name: land.name, city: land.city, street: land.street, availability: land.availability, element: card };
+    });
+    
+    // ------------------------------- Filter -------------------------------
+    const allLands = document.querySelector(".all-lands");
+    const availableLands = document.querySelector(".available-lands");
+    const unavailableLands = document.querySelector(".unavailable-lands");
+    
+    allLands.addEventListener("click", () => {
+        allLands.classList.add("option-item-active");
+        availableLands.classList.remove("option-item-active");
+        unavailableLands.classList.remove("option-item-active");
+        lands.forEach(land => {
+            land.element.classList.remove("hide");
+        });
+    });
+
+    availableLands.addEventListener("click", () => {
+        allLands.classList.remove("option-item-active");
+        availableLands.classList.add("option-item-active");
+        unavailableLands.classList.remove("option-item-active");
+        lands.forEach(land => {
+            if (land.availability === 1) {
+                land.element.classList.remove("hide");
+            } else {
+                land.element.classList.add("hide");
+            }
+        });
+    });
+    
+    unavailableLands.addEventListener("click", () => {
+        allLands.classList.remove("option-item-active");
+        availableLands.classList.remove("option-item-active");
+        unavailableLands.classList.add("option-item-active");
+        lands.forEach(land => {
+            if (land.availability === 0) {
+                land.element.classList.remove("hide");
+            } else {
+                land.element.classList.add("hide");
+            }
+        });
+    });
 </script>
 <?php require APPROOT.'/views/inc/footer.php'; ?>
