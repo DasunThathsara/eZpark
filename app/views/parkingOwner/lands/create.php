c006c9d4873868f2503fe083a1470c4bfc73b40d
C.D.Sriwarnasinghe <122434896+Damien08SRI@users.noreply.github.com>
2024-01-06 21:49:22 +0530

    change the wrap

--- app/views/parkingOwner/lands/create.php
+++ app/views/parkingOwner/lands/create.php
@@ -119,15 +119,15 @@ require APPROOT.'/views/inc/components/sidenavbar.php';
                     <div class="slots-container">
                         <div class="slots">
                             <div class="slots-title">Car</div>
-                            <input type="text" name="car" id="car" required value="<?php echo $data['car'] ?>" />
+                            <input type="number" name="car" id="car" required value="<?php echo $data['car'] ?>" />
                         </div>
                         <div class="slots">
                             <div class="slots-title">Bike</div>
-                            <input type="text" name="bike" id="bike" required value="<?php echo $data['bike'] ?>" />
+                            <input type="number" name="bike" id="bike" required value="<?php echo $data['bike'] ?>" />
                         </div>
                         <div class="slots">
                             <div class="slots-title">Three Wheel</div>
-                            <input type="text" name="threeWheel" id="threeWheel" required value="<?php echo $data['threeWheel'] ?>" />
+                            <input type="number" name="threeWheel" id="threeWheel" required value="<?php echo $data['threeWheel'] ?>" />
                         </div>
                     </div>  
 
