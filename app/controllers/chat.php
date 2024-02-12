aa13ff529aac8da39f310f05973ecc06bc4ff894
sohanihansika <136710534+sohanihansika@users.noreply.github.com>
2024-02-12 16:22:52 +0530

    chat controller

---
+++ app/controllers/chat.php
@@ -0,0 +1,16 @@
+<?php
+class Report extends Controller {
+    public function __construct(){
+        $this->middleware = new AuthMiddleware();
+        // Only parkingOwner are allowed to access driver pages
+        // $this->middleware->checkAccess(['parkingOwner']);
+        // $this->parkingOwnerModel = $this->model('parkingOwnerModel');
+    }
+
+    public function viewChat(){
+        $data = [
+            'title' => 'Home page'
+        ];
+        $this->view('chat/chat', $data);
+    }
+}
