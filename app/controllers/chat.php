7289281e231af5d9db66b902c3c0dc9b42aa543e
sohanihansika <136710534+sohanihansika@users.noreply.github.com>
2024-02-20 16:57:05 +0530

    controllers/chat.php --> loadUsers,insertMsg,getMsg

--- app/controllers/chat.php
+++ app/controllers/chat.php
@@ -3,6 +3,7 @@ class Chat extends Controller {
     public function __construct(){
         $this->middleware = new AuthMiddleware();
         $this->UserModel = $this->model('UserModel');
+        $this->ChatModel = $this->model('ChatModel');
         // Only parkingOwner are allowed to access driver pages
         // $this->middleware->checkAccess(['parkingOwner']);
         // $this->parkingOwnerModel = $this->model('parkingOwnerModel');
@@ -16,11 +17,37 @@ class Chat extends Controller {
         $this->view('chat/chat', $data);
     }
 
-    // public function viewMsg(){
-    //     $data = [
-    //         'title' => 'Home page',
-    //         'data' => $this->UserModel->viewUsersById(),
-    //     ];
-    //     $this->view('chat/msg', $data);
-    // }
+    public function loadUsers(){
+       $data = [
+            'title' => 'Home page',
+            'data' => $this->UserModel->viewUsersById(),
+        ];
+        // $this->view('chat/chat', $data);
+        echo json_encode($data);
+    }
+
+    public function viewMsg(){
+        $data = [
+            'title' => 'Home page',
+            'data' => $this->UserModel->viewUsersById(),
+        ];
+        $this->view('chat/msg', $data);
+    }
+
+    public function insertMsg(){
+        $data = [
+            'title' => 'Home page',
+            'data' => $this->ChatModel->insertChat(),
+        ];
+        echo json_encode($data);
+    }
+
+    public function getMsg(){
+        $data = [
+            'title' => 'Home page',
+            'data' => $this->ChatModel->getChat(),
+        ];
+        echo json_encode($data);
+        echo 'hi';
+    }
 }
