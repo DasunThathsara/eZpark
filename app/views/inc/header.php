2fe801a1ecb83a07effbaeb60a2ef472d797a66b
DasunThathsara <dasun.thathsara.sri@gmail.com>
2024-04-09 13:22:24 +0530

    test

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,13 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SITENAME ?></title>
-
     <!-- External CSS -->
     <link rel="stylesheet" href="<?php echo URLROOT ?>/css/style.css">
-
     <!-- External js -->
     <script src="<?php echo URLROOT ?>/js/script.js" defer></script>
-
     <!-- AJAX -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
@@ -20,54 +17,6 @@
 
     <!-- Chart.js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-
-    <style>
-        #map {
-            height: 225px;
-            width: 100%;
-        }
-
-        .map{
-            display: none;
-            border-radius: 5px;
-        }
-
-        .map-active{
-            display: block;
-        }
-
-        .map-btn{
-            background-color: #e09b01;
-            color: white;
-            display: none;
-            text-align: center;
-            padding: 10px;
-            border-radius: 5px;
-            margin-top: 5px;
-        }
-
-        .map-btn-active{
-            display: block;
-        }
-
-        .map-details{
-            display: block;
-        }
-
-        .map-details-hide{
-            display: none;
-        }
-
-        .map-trigger-btn{
-            cursor: pointer;
-            background-color: #e29c00;
-            padding: 12px;
-            border-radius: 5px;
-            color: white;
-            text-align: center;
-        }
-
-    </style>
 </head>
 <body>
 
