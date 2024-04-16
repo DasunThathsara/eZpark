46e4f5e0aa423bfba6e15e524a78f202bccafb33
sohanihansika <136710534+sohanihansika@users.noreply.github.com>
2024-04-16 13:02:07 +0530

    merchandiser index- recent transactions

--- app/views/merchandiser/index.php
+++ app/views/merchandiser/index.php
@@ -95,64 +95,64 @@
 
         <div class="open-side-cards-btn" onclick="closeRightCard()">View Transaction</div>
 
-        <div class="side-cards">
+        <div id="side-cards-container" class="side-cards">
             <div class="close-btn" onclick="closeRightCard()">X</div>
             <h2>Recent Transaction</h2>
 
             <p><span>&#9632;</span>Today</p>
 
-            <div class="side-card">
-                <div class="date-time">2023.11.22</div>
-                <div class="parking">Nolimit</div>
-                <div class="transaction-type in">In</div>
-            </div>
+            <?php foreach ($data['today_total_transactions'] as $transaction): ?>
+                <div class="side-card">
+                    <div class="date-time">
+                        <?php
+                        $dateTime = new DateTime($transaction->transactionTime);
+                        $time = $dateTime->format('H:i:s');
+                        echo $time;
+                        ?>
+                    </div>
+                    <div class="vehicle-type">
+                        <?php if($transaction->vehicleType == 'bike'): ?>
+                            <img style="width: 30px;" src="<?php echo URLROOT ?>/images/motor-sports.png" alt="">
+                        <?php elseif($transaction->vehicleType == 'car'): ?>
+                            <img style="width: 30px;" src="<?php echo URLROOT ?>/images/car-c.png" alt="">
+                        <?php elseif($transaction->vehicleType == 'threeWheel'): ?>
+                            <img style="width: 30px;" src="<?php echo URLROOT ?>/images/tuk-tuk.png" alt="">
+                        <?php endif; ?>
+                    </div>
+                    <div class="vehicle"><?php echo $transaction->name?></div>
+                    <?php if($transaction->status == 1): ?>
+                        <div class="transaction-type in">In</div>
+                    <?php else: ?>
+                        <div class="transaction-type out">Out</div>
+                    <?php endif; ?>
+                </div>
+            <?php endforeach;?> 
 
-            <div class="side-card">
-                <div class="date-time">2023.11.22</div>
-                <div class="parking">Keels</div>
-                <div class="transaction-type out">Out</div>
-            </div>
-
-            <div class="side-card">
-                <div class="date-time">2023.11.22</div>
-                <div class="parking">Nolimit</div>
-                <div class="transaction-type in">In</div>
-            </div>
-
-            <div class="side-card">
-                <div class="date-time">2023.11.22</div>
-                <div class="parking">Keels</div>
-                <div class="transaction-type out">Out</div>
-            </div>
-
-            <p><span>&#9632;</span>Yesterday</p>
-
-            <div class="side-card">
-                <div class="date-time">2023.11.22</div>
-                <div class="parking">Nolimit</div>
-                <div class="transaction-type in">In</div>
-            </div>
-
-            <div class="side-card">
-                <div class="date-time">2023.11.22</div>
-                <div class="parking">Keels</div>
-                <div class="transaction-type out">Out</div>
-            </div>
-
-            <div class="side-card">
-                <div class="date-time">2023.11.22</div>
-                <div class="parking">Nolimit</div>
-                <div class="transaction-type in">In</div>
-            </div>
-
-            <div class="side-card">
-                <div class="date-time">2023.11.22</div>
-                <div class="parking">Keels</div>
-                <div class="transaction-type out">Out</div>
-            </div>
+            
         </div>
     </section>
 </main>
+
+<script>
+        // --------------------------------------- Real time update transaction box ---------------------------------------
+        function refreshSideCard() {
+            // Fetch updated content via AJAX
+            fetch('<?php echo URLROOT?>/merchandiser/index')
+                .then(response => response.text())
+                .then(data => {
+                    // Replace only the content inside the side-cards container
+                    document.getElementById('side-cards-container').innerHTML =
+                        document.createRange().createContextualFragment(data).querySelector('.side-cards').innerHTML;
+                })
+                .catch(error => console.error('Error fetching data:', error));
+        }
+
+        // Refresh every 1 second
+        setInterval(refreshSideCard, 1000);
+
+
+        
+    </script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 <script>
