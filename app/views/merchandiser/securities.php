b525b0b9a660174acf898f04b4d27f9e0e6fc5e9
C.D.Sriwarnasinghe <122434896+Damien08SRI@users.noreply.github.com>
2024-04-18 10:50:10 +0530

    security access by merchandiser

--- app/views/merchandiser/securities.php
+++ app/views/merchandiser/securities.php
@@ -40,19 +40,32 @@ require APPROOT.'/views/inc/components/sidenavbar.php';
                             </th>
                         </tr>
 
-                        <?php for ($i = 0; $i < sizeof($other_data) - 1; $i++) {?>
+   
+
+<?php foreach ($data['securityDetails'] as $security) {?>
                             <tr>
                                 <td>
-                                    <a class="tile" href="">
+                                    <a class="tile" href="<?php echo URLROOT?>/land/viewSecurity/<?php echo $security->security_id?>">
                                         <div class="content">
                                             <div class="left" style="width:50%;">
-                                                <?php echo $other_data[$i]->name ?>
+                                                <?php echo $security->security_name; ?>
                                             </div>
+                                            <div class="left" style="width:50%;">
+                                                <?php echo $security->sec_contact; ?>
+                                            </div>
+
+                                            <div class="toggle">
+                                                <!-- Toggle Button -->
+                                                <label class="switchAccess" id="switch">
+                                                    <input type="checkbox" hidden class="toggleButton" data-security-id="<?php echo $security->security_id;?>" <?php echo $security->landAccess == 1 ? 'checked' : ''; ?>>
+                                                    <span class="slider round"></span>
+                                                </label>
+                                            </div>    
                                             <div class="right" style="width: calc(50% - 30px);">
-                                                <form action="<?php echo URLROOT ?>/security/securityRemove" method="post">
-                                                    <input type="text" name="package_type" id="package_type" hidden value="<?php echo $other_data[$i]->id ?>" />
-                                                    <input type="text" name="id" id="id" hidden value="<?php echo $data['id'] ?>" />
-                                                    <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <form action="<?php echo URLROOT ?>/merchandiser/securityRemove" method="post" class="delete-form" id="delete-form">
+                                                    <input type="text" name="sec_id" id="sec_id" value="<?php echo $security->security_id; ?>" hidden />
+                                                    <input type="text" name="land_id" id="land_id" value="<?php echo $data['id']; ?>" hidden />
+                                                    <button type="submit" class="delete" onclick="confirmSubmit();">
                                                         <img src="<?php echo URLROOT ?>/images/trash-solid.svg" alt="">
                                                     </button>
                                                 </form>
@@ -69,10 +82,80 @@ require APPROOT.'/views/inc/components/sidenavbar.php';
     </section>
 </main>
 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
 <script>
-    function confirmSubmit() {
-        return confirm("Are you sure you want to delete this vehicle?");
-    }
+    document.addEventListener("DOMContentLoaded", function () {
+        const deleteForms = document.querySelectorAll(".delete-form");
+
+        deleteForms.forEach(function (deleteform) {
+            const submitButton = deleteform.querySelector("button[type='submit']");
+
+            if (submitButton) {
+                submitButton.addEventListener("click", function (event) {
+                    event.preventDefault(); // Prevent the form from submitting
+
+                    // Use SweetAlert for confirmation
+                    Swal.fire({
+                        title: 'Are you sure?',
+                        text: 'You are about to delete this security.',
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete!'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            deleteform.submit();
+                        }
+                    });
+                });
+            }
+        });
+    });
+</script>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const toggleButtons = document.querySelectorAll(".toggleButton");
+
+        toggleButtons.forEach(function (toggleButton) {
+            toggleButton.addEventListener("change", function () {
+                const isChecked = this.checked;
+                const securityId = this.getAttribute("data-security-id");
+
+                // Use SweetAlert for confirmation
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: 'You are about to change security access.',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, proceed!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // Perform the AJAX request to update security access
+                        $.ajax({
+                            url: '<?php echo URLROOT?>/merchandiser/landAccessControl/' + securityId,
+                            method: 'GET',
+                            data: { isChecked: isChecked },
+                            success: function (response) {
+                                console.log("AJAX success:", response);
+                            },
+                            error: function (xhr, status, error) {
+                                console.error("AJAX error:", xhr.responseText);
+                            }
+                        });
+                    } else {
+                        // Revert the checkbox state if the user cancels
+                        toggleButton.checked = !isChecked;
+                    }
+                });
+            });
+        });
+    });
+    
 </script>
 
 <?php require APPROOT.'/views/inc/footer.php'; ?>
